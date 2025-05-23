a55c32d5ce2e8af77b33770c57664ad6d1696196
DoroDore <tlyz82@student.cis.eduhk>
2025-05-23 18:27:25 +0800

    Completed

---
+++ ScoreKeeper/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  ScoreKeeper
+//
+//  Created by Theodore Zhu on 5/23/25.
+//
+
+import Foundation
+struct Player: Identifiable {
+    let id = UUID()
+    var name: String
+    var score: Int
+    
+}
