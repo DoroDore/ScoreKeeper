050e7639dcb735af021a55a537f4eda7a3c101c1
DoroDore <tlyz82@student.cis.eduhk>
2025-05-23 08:20:31 +0800

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Theodore Zhu on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoreKeeperTests/ScoreKeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  ScoreKeeperTests.swift
+//  ScoreKeeperTests
+//
+//  Created by Theodore Zhu on 5/23/25.
+//
+
+import Testing
+
+struct ScoreKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
